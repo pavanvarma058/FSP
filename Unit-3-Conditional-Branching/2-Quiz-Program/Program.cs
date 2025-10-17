1178783f6dd98dc8b63d450a4e5c9934459db88e
Pavan Varma <pavanvarma058@gmail.com>
2025-10-17 16:44:46 +0100

    Completed Unit-3 exercises from the course work

---
+++ Unit-3-Conditional-Branching/2-Quiz-Program/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace _2_Quiz_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Welcome to the Quiz");
+
+            Console.WriteLine();
+            // Question 1
+            Console.WriteLine("Q1: What is 2x2?");
+            Console.WriteLine("2");
+            Console.WriteLine("4");
+            Console.WriteLine("8");
+            Console.Write("Enter your answer: ");
+            int ans1 = int.Parse(Console.ReadLine());
+
+            if (ans1 == 4)
+            {
+                count++;
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect");
+            }
+            Console.WriteLine();
+            // Question 2
+            Console.WriteLine("Q2: What is 5x9?");
+            Console.WriteLine("42");
+            Console.WriteLine("48");
+            Console.WriteLine("45");
+            Console.Write("Enter your answer: ");
+            int ans2 = int.Parse(Console.ReadLine());
+
+            if (ans2 == 45)
+            {
+                count++;
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect");
+            }
+            Console.WriteLine();
+            // Question 3
+            Console.WriteLine("Q3: What is 95-45?");
+            Console.WriteLine("40");
+            Console.WriteLine("-25");
+            Console.WriteLine("50");
+            Console.Write("Enter your answer: ");
+            int ans3 = int.Parse(Console.ReadLine());
+
+            if (ans3 == 50)
+            {
+                count++;
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect");
+            }
+
+            Console.WriteLine();
+
+            if (count == 3)
+            {
+                Console.WriteLine("You have answered 3 questions correctly");
+            }
+            else if (count == 2)
+            {
+                Console.WriteLine("You have answered 2 questions correctly");
+            }
+            else if (count == 1)
+            {
+                Console.WriteLine("You have answered 1 question correctly");
+            }
+            else
+            {
+                Console.WriteLine("You have answered 0 question correctly");
+            }
+        }
+    }
+}
---
+++ Unit-3-Conditional-Branching/3-Cinema-Program-Revisited/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace _3_Cinema_Program_Revisited
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3. Cinema Program (Revisited)
+            Console.WriteLine("Welcome to NTU cinema!");
+            Console.WriteLine();
+
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Currently Showing:");
+            Console.WriteLine("1. The Imitation Game (12)");
+            Console.WriteLine("2. Ada (13)");
+            Console.WriteLine("3. Steve Jobs (15)");
+
+            Console.WriteLine();
+
+            // customer choosing the movie
+            Console.Write("Which film you would like to see: ");
+            Console.WriteLine();
+            int film = int.Parse(Console.ReadLine());
+
+            if (film == 1)
+            {
+                Console.WriteLine("The Imitation Game (12)");
+                Console.WriteLine();
+                if (age >= 12)
+                {
+                    Console.WriteLine("Enjoy the film");
+                }
+                else
+                {
+                    Console.WriteLine("You are too young too watch this film");
+                }
+            }
+            else if (film == 2)
+            {
+                Console.WriteLine("Ada (13)");
+                Console.WriteLine();
+                if (age >= 12)
+                {
+                    Console.WriteLine("Enjoy the film");
+                }
+                else
+                {
+                    Console.WriteLine("You are too young too watch this film");
+                }
+            }
+            else if (film == 3)
+            {
+                Console.WriteLine("Steve Jobs (15)");
+                Console.WriteLine();
+                if (age >= 12)
+                {
+                    Console.WriteLine("Enjoy the film");
+                }
+                else
+                {
+                    Console.WriteLine("You are too young too watch this film");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The film you're asked is not screening now");
+            }
+
+            Console.WriteLine();
+
+            // Popcorn
+            Console.Write("Do you like to have some popcorn? ");
+            string customer = Console.ReadLine();
+
+            if (customer == "Y")
+            {
+                Console.WriteLine("Enjoy your popcorn");
+            }
+            else if (customer == "N")
+            {
+                Console.WriteLine("Thank you!!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
