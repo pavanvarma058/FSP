a333d9e84c5ae8f102774d07960891ebf4e7fabb
Pavan Varma <pavanvarma058@gmail.com>
2025-10-22 23:40:06 +0100

    Completed one exercise in Unit-7 exercise

---
+++ Unit-7-Procedures/1-Defining-A-Procedures/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _1_Defining_A_Procedures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            greetings();
+        }
+    }
+        // Defining a procedure
+        static void greetings()
+        {
+            Console.WriteLine("Hello, Good Morning!");
+        }
+    }
