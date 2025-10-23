a3c6441cf7c15d69e9b3bd9f9893271ec49a320a
Pavan Varma <pavanvarma058@gmail.com>
2025-10-23 23:04:28 +0100

    Added Dice simulator program to Unit-7 from the course work exercise

---
+++ Unit-7-Procedures/2-Dice-Simulator/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _2_Dice_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dice Simulator
+            Console.WriteLine("Welcome to Dice Simulator Game :)");
+
+            // Single Dice rolled
+            rollDice();
+            rollDice();
+            rollDice();
+
+            Console.WriteLine();
+
+            // Two Dice rolled
+            twoDiceRolled();
+            twoDiceRolled();
+            twoDiceRolled();
+
+            Console.ReadLine();
+        }
+        static void rollDice()
+        {
+            Random num = new Random();
+            int number = num.Next(1, 6);
+            Console.WriteLine("Dice rolled: " + number);
+        }
+
+        static void twoDiceRolled()
+        {
+            Random random = new Random();
+            int number1 = random.Next(1, 6);
+            int number2 = random.Next(1, 6);
+            int total = number1 + number2;
+            Console.WriteLine("Two Dice rolled: " + total);
+        }
+    }
+}
