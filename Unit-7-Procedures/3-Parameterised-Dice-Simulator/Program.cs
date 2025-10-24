c1324336371f82812aa68aa154ad117414d57309
Pavan Varma <pavanvarma058@gmail.com>
2025-10-24 17:48:41 +0100

    Completed 3 & 4 exercises from the course work

---
+++ Unit-7-Procedures/3-Parameterised-Dice-Simulator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _3_Parameterised_Dice_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parameterised Dice Simulator
+            rollDice(2);
+            rollDice(5);
+            rollDice(20);
+
+            Console.ReadLine();
+        }
+        static void rollDice(int numberOfTimes)
+        {
+            int total = 0;
+            for (int i = 1; i <= numberOfTimes; i++)
+            {
+                Random random = new Random();
+                total += random.Next(1, 6);
+            }
+            Console.WriteLine(numberOfTimes + " dice rolled: " + total);
+        }
+    }
+}
---
+++ Unit-7-Procedures/4-Railway-Train-Departures/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _4_Railway_Train_Departures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Railway Train Departures
+            string[] stationCodes = { "LON", "NOT", "BHM", "MAN", "CDF", "EDB", "GLC", "LDS" };
+
+            int[] scheduledHours = { 08, 09, 11, 13, 15, 18, 21, 23 };
+
+            int[] scheduledMinutes = { 05, 45, 23, 01, 27, 05, 48, 55 };
+
+            int[] lateMinutes = { 8, 0, 37, 6, 0, 0, 20, 9 };
+
+            Console.WriteLine("Destination   Scheduled   Expected");
+
+            for (int i = 0; i < stationCodes.Length; ++i)
+            {
+                Console.Write("    ");
+                Console.Write(stationCodes[i]);
+                Console.Write("         ");
+                writeTime(scheduledHours[i], scheduledMinutes[i]);
+                Console.Write("      ");
+
+                int expectedMinutes = scheduledMinutes[i] + lateMinutes[i];
+                writeTime(scheduledHours[i], expectedMinutes);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        // 3. Improving Code Quality with a Procedure
+        // 4. Displaying Time
+        static void writeTime(int hours, int minutes)
+        {
+            int actualMinutes = minutes % 60;
+            int actualHours = (hours + (minutes / 60)) % 24;
+
+            string hoursFormat = string.Format("{0:00}", actualHours);
+            string minutesFormat = string.Format("{0:00}", actualMinutes);
+            Console.Write(hoursFormat + ":" + minutesFormat);
+        }
+    }
+}
