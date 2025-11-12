fba225e68deb90ded0a5fce375bf7168117e5626
Pavan Varma <pavanvarma058@gmail.com>
2025-11-12 23:21:15 +0000

    Added Unit-5 exercises

---
+++ Unit-5-Alternative-Loop-Control/1-Ten-Green-Bottles/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _1_Ten_Green_Bottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ten Greeen Bottles 
+            int bottles = 10;
+
+            // Used for loop and ternary opeartor
+            for(int i=bottles - 1; i>=0; i--)
+            {
+                string verse = bottles + " green bottle" + (bottles == 1 ? "" : "s") +" sitting on the wall,\n"
+                    + bottles + " green bottle" + (bottles == 1 ? "" : "s") + 
+                    " sitting on the wall\nand if one green bottle should accidentally fall,\nthere will be " 
+                    + i + " green bottle" + (i==1 ? "" : "s") + " sitting on the wall";
+                Console.WriteLine(verse);
+                bottles--;
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Unit-5-Alternative-Loop-Control/2-Configuring-the-rhyme/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _2_Configuring_the_rhyme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3.Configuring the Rhyme
+
+            // Asking the user to input number of bottles and choose a colour other than green
+            Console.Write("Enter the number of bottles: ");
+            int bottles = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Choose a colour: ");
+            string colour = Console.ReadLine();
+
+            Console.WriteLine();
+
+            // Used for loop and ternary opeartor
+            for (int i = bottles - 1; i >= 0; i--)
+            {
+                string verse = bottles + " " + colour + " bottle" + (bottles == 1 ? "" : "s") + " sitting on the wall,\n"
+                    + bottles + " " + colour + " bottle" + (bottles == 1 ? "" : "s") +
+                    " sitting on the wall\nand if one " + colour + " bottle should accidentally fall,\nthere will be "
+                    + i + " " + colour + " bottle" + (i == 1 ? "" : "s") + " sitting on the wall";
+                Console.WriteLine(verse);
+                bottles--;
+                Console.WriteLine();
+            }
+        }
+    }
+}
